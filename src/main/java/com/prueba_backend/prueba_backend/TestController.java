2c54fc9515d23f4f6da82593ddf4ba6201471cc9
Gorka Ruiz <48318268+Gorka29@users.noreply.github.com>
2024-12-14 19:49:37 +0100

    Update TestController.java

--- src/main/java/com/prueba_backend/prueba_backend/TestController.java
+++ src/main/java/com/prueba_backend/prueba_backend/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
+@CrossOrigin(origins = "http://localhost:4200") // Habilita CORS para tu frontend
 public class TestController {
 
     @GetMapping("/hello")
